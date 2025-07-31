189c368198eb20db7760fab619dbdf68183c3608
MohamadSolkhanNawawi <mohamadsolkhan558@gmail.com>
2025-07-31 22:05:24 +0700

    Menggunakan data seed, belum menggunakan database

--- laravel-backend/database/migrations/2025_07_31_082140_create_service_categories_table.php
+++ laravel-backend/database/migrations/2025_07_31_082140_create_service_categories_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('service_categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->text('description')->nullable();
+            $table->string('icon')->nullable();
+            $table->string('color', 7)->nullable();
             $table->timestamps();
         });
     }
--- laravel-backend/database/seeders/DatabaseSeeder.php
+++ laravel-backend/database/seeders/DatabaseSeeder.php
@@ -13,11 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Baris ini memanggil seeder yang kita buat
+        $this->call([
+            PublicServiceSeeder::class,
         ]);
     }
 }
